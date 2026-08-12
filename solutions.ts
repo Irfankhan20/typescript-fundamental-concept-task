c79142eef2f2a7feed99e98265893f18b5ecc0e7
Irfankhan20 <sipathan96@gmail.com>
2026-08-12 11:41:16 +0600

    assignment one

---
+++ solutions.ts
@@ -0,0 +1,7 @@
+// problem 01
+const numbers: number[] = [1, 2, 3, 4, 5, 6];
+const filterEvenNumbers: number[] = numbers.filter((num) => num % 2 === 0);
+
+console.log(filterEvenNumbers);
+
+// problem 02
